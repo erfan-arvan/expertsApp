e4948a674a86ab9db000a3f1eab167c419d9c374
Erfan Arvan <ea442@njit.edu>
2025-05-19 09:42:29 -0400

    reverse some config changes

--- ServerApp/src/main/java/com/github/erfanarvan/serverapp/Main.java
+++ ServerApp/src/main/java/com/github/erfanarvan/serverapp/Main.java
@@ -2,31 +2,10 @@ package com.github.erfanarvan.serverapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
-
-import com.github.erfanarvan.serverapp.config.AppConfig;
-
 
 @SpringBootApplication
-@EnableConfigurationProperties(AppConfig.class)
-public class Main implements org.springframework.boot.CommandLineRunner {
-
-    private final AppConfig appConfig;
-
-    public Main(AppConfig appConfig) {
-        this.appConfig = appConfig;
-    }
-
+public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
-
-    @Override
-    public void run(String... args) {
-        // Debugging puroses
-        System.out.println("Frontend URL from AppConfig: " + appConfig.getFrontendUrl());
-        System.out.println("Backend URL from AppConfig: " + appConfig.getBackendUrl());
-        System.out.println("Production URL from AppConfig: " + appConfig.getProductionUrl());
-    }
-
 }
