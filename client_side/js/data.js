c28b398c9f9e678b3976077ee78fa25b56f44e76
Erfan Arvan <ea442@njit.edu>
2025-05-27 00:36:53 -0400

    students page

--- client_side/js/data.js
+++ client_side/js/data.js
@@ -507,7 +507,7 @@ const snippets = [
       const StartingQuestions = {
         "type": "none",
         "id": "Read",
-        "prompt": "Please read and understand this code snippet, and click on next when you feel you are ready to answer questions about it.",
+        "prompt": "Please read the code snippet and form an overall impression. This should take no more than 2 minutes. Click 'Next' when you feel ready to answer the questions. (You will still see the snippet while answering the questions.)",
         "type2": "",
         "id2": "",
         "question2": ""
