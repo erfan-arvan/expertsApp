f3e53aa985e5f773a015e05933b26e6e62380e65
Erfan Arvan <ea442@njit.edu>
2025-06-02 09:01:01 -0400

    tests for adding javadocs

--- client_side/js/dataTest.js
+++ client_side/js/dataTest.js
@@ -3,24 +3,24 @@ const snippetsOriginal = [
       id: 1,
       title: "Snippet 1",
               code:`
-<span class="keyword">public static</span> <span class="type">boolean</span> <span class="function">methodA</span>(<span class="type">String</span> name) {
-      <span class="keyword">if</span> (name == <span class="literal">null</span>) {
-        <span class="keyword">return</span> <span class="literal">false</span>;
-      }
-      <span class="keyword">if</span> (name.<span class="function">startsWith</span>(<span class="string">"."</span>)) {
-        <span class="keyword">return</span> <span class="literal">false</span>;
-      }
-      <span class="keyword">if</span> ((name.<span class="function">length</span>() < <span class="number">1</span>) || (name.<span class="function">length</span>() > MAX_NAME_LENGTH)) {
-        <span class="keyword">return</span> <span class="literal">false</span>;
-      }
-      <span class="keyword">for</span> (<span class="type">int</span> i = <span class="number">0</span>; i < name.<span class="function">length</span>(); i++) {
-        <span class="type">char</span> c = name.<span class="function">charAt</span>(i);
-        <span class="keyword">if</span> (!Character.<span class="function">isLetterOrDigit</span>(c) && !VALID_NAME_SET.<span class="function">contains</span>(c)) {
+      <span class="keyword">public static</span> <span class="type">boolean</span> <span class="function">methodA</span>(<span class="type">String</span> name) {
+        <span class="keyword">if</span> (name == <span class="literal">null</span>) {
           <span class="keyword">return</span> <span class="literal">false</span>;
         }
+        <span class="keyword">if</span> (name.<a href="#" class="function" onclick="openJavadocModal('https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#startsWith-java.lang.String-')">startsWith</a>(<span class="string">"."</span>)) {
+          <span class="keyword">return</span> <span class="literal">false</span>;
+        }
+        <span class="keyword">if</span> ((name.<a href="#" class="function" onclick="openJavadocModal('https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#length--')">length</a>() < <span class="number">1</span>) || (name.<a href="#" class="function" onclick="openJavadocModal('https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#length--')">length</a>() > MAX_NAME_LENGTH)) {
+          <span class="keyword">return</span> <span class="literal">false</span>;
+        }
+        <span class="keyword">for</span> (<span class="type">int</span> i = <span class="number">0</span>; i < name.<a href="#" class="function" onclick="openJavadocModal('https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#length--')">length</a>(); i++) {
+          <span class="type">char</span> c = name.<a href="#" class="function" onclick="openJavadocModal('https://docs.oracle.com/javase/8/docs/api/java/lang/String.html#charAt-int-')">charAt</a>(i);
+          <span class="keyword">if</span> (!Character.<a href="#" class="function" onclick="openJavadocModal('https://docs.oracle.com/javase/8/docs/api/java/lang/Character.html#isLetterOrDigit-char-')">isLetterOrDigit</a>(c) && !VALID_NAME_SET.<a href="#" class="function" onclick="openJavadocModal('https://docs.oracle.com/javase/8/docs/api/java/util/Set.html#contains-java.lang.Object-')">contains</a>(c)) {
+            <span class="keyword">return</span> <span class="literal">false</span>;
+          }
+        }
+        <span class="keyword">return</span> <span class="literal">true</span>;
       }
-      <span class="keyword">return</span> <span class="literal">true</span>;
-    }
       `
           },
           {
